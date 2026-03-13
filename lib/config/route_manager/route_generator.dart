4ac71c718014d56e6553f8252babd0ef4583109c
Mohamed Mousa <mousa1152003@gmail.com>
2026-03-13 14:37:15 +0200

    Implement bottom navigation bar
    - Add `flutter_svg` dependency and update `pubspec.lock`.
    - Create `CustomBottomNavBar` shared widget to manage navigation between Home, Result, and Profile screens.
    - Implement `HomeScreen`, `ProfileScreen`, and `ResultScreen` presentation pages.
    - Configure `BottomNavigationBarThemeData` in `AppTheme`.
    - Add SVG assets for navigation icons and update `AppAssets` utility.
    - Update `RouteGenerator` to use `CustomBottomNavBar` as the main home route.
    - Refactor `Routes` constants to remove redundant individual screen routes.
    - Update `widget_test.dart` and `ProfileScreen` to handle authentication token logic.

--- lib/config/route_manager/route_generator.dart
+++ lib/config/route_manager/route_generator.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:online_exam/config/route_manager/routes.dart';
+import '../../core/shared_widgets/custom_bottom_nav.dart';
 import 'package:online_exam/features/auth/presention/pages/register/register_screen.dart';
 import '../../core/shared_widgets/custom_bottom_nav.dart';
 import '../../features/auth/presentation/pages/login/login_screen.dart';
@@ -12,6 +13,8 @@ class RouteGenerator {
       /// Login Screen
       case Routes.loginRoute:
         return CupertinoPageRoute(builder: (_) => LoginScreen(),);
+        case Routes.homeRoute:
+        return CupertinoPageRoute(builder: (_) => CustomBottomNavBar(),);
 
       case Routes.homeRoute:
         return CupertinoPageRoute(builder: (_) => CustomBottomNavBar(),);
