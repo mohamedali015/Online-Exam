5738f45506079f924ef23417c895dbbc5025c1d1
Mohamed Abdelrahman Mousa <167023637+mohamedmousa2003@users.noreply.github.com>
2026-03-14 05:45:49 +0200

    Revert "Feature/register"

--- lib/core/utils/app_constants.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-abstract class AppConstants {
-  // static const String fontFamily = 'Cairo';
-  // static const paddingHorizontal = 16.0;
-}
--- lib/core/values/api_end_points.dart
+++
@@ -1,4 +0,0 @@
-abstract class ApiEndPoints {
-  static const String baseUrl = "https://exam.elevateegy.com/api/v1";
-  static const String register = "$baseUrl/auth/signup";
-}
