bf95aceb7e41dcf05a615adf0b121d6b19c1b89d
Mohamed Ali <mohamed.ali01537@gmail.com>
2026-03-29 08:11:07 +0200

    fix: clean up route generator by removing redundant imports and simplifying route return statements

--- lib/config/route_manager/route_generator.dart
+++ lib/config/route_manager/route_generator.dart
@@ -4,28 +4,22 @@ import 'package:online_exam/config/route_manager/routes.dart';
 import '../../core/shared_widgets/custom_bottom_nav_bar.dart';
 import 'package:online_exam/features/auth/presention/pages/register/register_screen.dart';
 import '../../features/splash/splash_screen.dart';
-import '../../core/shared_widgets/custom_bottom_nav_bar.dart';
-import '../../features/auth/presentation/pages/login/login_screen.dart';
-import '../../features/splash/splash_screen.dart';
-
 import '../../features/auth/presention/pages/login/login_screen.dart';
 
 import 'package:online_exam/features/forget_password/presentation/widgets/forget_password_flow.dart';
 
 class RouteGenerator {
-
   static Route<dynamic> getRoute(RouteSettings settings) {
     switch (settings.name) {
-
       /// Splash Screen
       case Routes.splashRoute:
-        return CupertinoPageRoute(builder: (_) => const SplashScreen(),);
+        return CupertinoPageRoute(builder: (_) => const SplashScreen());
 
-     /// Login Screen
+      /// Login Screen
       case Routes.loginRoute:
-        return CupertinoPageRoute(builder: (_) => LoginScreen(),);
-        case Routes.homeRoute:
-        return CupertinoPageRoute(builder: (_) => CustomBottomNavBar(),);
+        return CupertinoPageRoute(builder: (_) => LoginScreen());
+      case Routes.homeRoute:
+        return CupertinoPageRoute(builder: (_) => CustomBottomNavBar());
 
       /// Register Screen
       case Routes.registerRoute:
@@ -37,22 +31,17 @@ class RouteGenerator {
           settings: settings,
         );
 
-    /// Default (Unknown Route)
+      /// Default (Unknown Route)
       default:
         return _errorRoute();
     }
-
   }
-
 
   static Route<dynamic> _errorRoute() {
     return CupertinoPageRoute(
       builder: (_) => const Scaffold(
         body: Center(
-          child: Text(
-            'Page Not Found',
-            style: TextStyle(fontSize: 18),
-          ),
+          child: Text('Page Not Found', style: TextStyle(fontSize: 18)),
         ),
       ),
     );
