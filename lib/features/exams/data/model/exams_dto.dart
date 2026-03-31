c10c0549b1627186371ad8195f518071d66c54ef
sarah elkholy <saraelkholyarmy03@gmail.com>
2026-03-31 16:44:12 +0200

    - created exams api client - created exams remote data source (contract-impl) - created exams models (response-meta_data-exams_dto) - created exams repo (contract-impl)

---
+++ lib/features/exams/data/model/exams_dto.dart
@@ -0,0 +1,47 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:online_exam/features/exams/domain/entities/exam_model.dart';
+part 'exams_dto.g.dart';
+
+@JsonSerializable()
+class Exams {
+  @JsonKey(name: "_id")
+  final String? Id;
+  @JsonKey(name: "title")
+  final String? title;
+  @JsonKey(name: "duration")
+  final int? duration;
+  @JsonKey(name: "subject")
+  final String? subject;
+  @JsonKey(name: "numberOfQuestions")
+  final int? numberOfQuestions;
+  @JsonKey(name: "active")
+  final bool? active;
+  @JsonKey(name: "createdAt")
+  final String? createdAt;
+
+  Exams({
+    this.Id,
+    this.title,
+    this.duration,
+    this.subject,
+    this.numberOfQuestions,
+    this.active,
+    this.createdAt,
+  });
+
+  ExamsModel toEntity() {
+    return ExamsModel(
+      title: title ?? '',
+      duration: duration ?? 0,
+      numberOfQuestions: numberOfQuestions ?? 0,
+    );
+  }
+
+  factory Exams.fromJson(Map<String, dynamic> json) {
+    return _$ExamsFromJson(json);
+  }
+
+  Map<String, dynamic> toJson() {
+    return _$ExamsToJson(this);
+  }
+}
---
+++ lib/features/exams/data/repositories/exam_repo_impl.dart
@@ -0,0 +1,27 @@
+import 'package:injectable/injectable.dart';
+import 'package:online_exam/config/error_handling/result.dart';
+import 'package:online_exam/features/exams/data/data_source/exams_remote_data_source.dart';
+import 'package:online_exam/features/exams/data/model/exams_dto.dart';
+import 'package:online_exam/features/exams/domain/entities/exam_model.dart';
+import 'package:online_exam/features/exams/domain/repositories/exams_repo.dart';
+
+@Injectable(as: ExamsRepo)
+class ExamsRepoImpl implements ExamsRepo {
+  final ExamsRemoteDataSource _examsRemoteDataSource;
+  ExamsRepoImpl(this._examsRemoteDataSource);
+  @override
+  Future<Result<List<ExamsModel>>> getSubjectExams({
+    required String subjectId,
+  }) async {
+    final response = await _examsRemoteDataSource.getSubjectExams(subjectId);
+    switch (response) {
+      case Success<List<Exams>>():
+        return Success<List<ExamsModel>>(
+          response.data.map((dto) => dto.toEntity()).toList(),
+        );
+
+      case Failure<List<Exams>>():
+        return Failure<List<ExamsModel>>(response.errorMessage);
+    }
+  }
+}
