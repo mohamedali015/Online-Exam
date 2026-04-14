737a19f1312582877111567ce55b6c3546d98908
sarah elkholy <saraelkholyarmy03@gmail.com>
2026-04-14 16:12:52 +0200

    - edit exam entity

--- lib/features/exams/domain/entities/exam_model.dart
+++ lib/features/exams/domain/entities/exam_model.dart
@@ -1,11 +1,13 @@
 import 'package:equatable/equatable.dart';
 
 class ExamsModel extends Equatable {
+  final String id;
   final String title;
   final int duration;
   final int numberOfQuestions;
 
   const ExamsModel({
+    required this.id,
     required this.title,
     required this.duration,
     required this.numberOfQuestions,
