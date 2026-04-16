171c030bdc09c94be23a871270f86b64a3211bfc
Mohamed Ali <mohamed.ali01537@gmail.com>
2026-04-16 16:57:13 +0200

    fix: remove hardcoded color from CircularProgressIndicator in CustomButton

--- lib/core/shared_widgets/custom_button.dart
+++ lib/core/shared_widgets/custom_button.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:online_exam/core/helpers/my_responsive.dart';
-import 'package:online_exam/core/utils/app_colors.dart';
 import '../utils/app_text_styles.dart';
 
 class CustomButton extends StatelessWidget {
@@ -41,7 +40,7 @@ class CustomButton extends StatelessWidget {
           ? SizedBox(
               width: MyResponsive.width(value: 22),
               height: MyResponsive.height(value: 22),
-              child: CircularProgressIndicator(color: AppColors.baseWhite),
+              child: CircularProgressIndicator(),
             )
           : Text(
               title,
